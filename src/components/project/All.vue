0a529efe38d1ec1435c4db54ce6d7da2f236b6bb
Cécile <gaulph@gmail.com>
2021-02-15 21:34:59 +0100

    all json -2

--- src/components/project/All.vue
+++ src/components/project/All.vue
@@ -9,122 +9,12 @@
 
       <div id="list" v-for="info in donnees" :key="info.id">
         <div>
-          <h4>{{info.title}}</h4>
+          <h4 class="mt-5">{{info.title}}</h4>
           <ul>
             <li v-for="pro in info.projects" :key="pro.id">{{pro.text}}</li>
           </ul>
+          <p><b>Total : </b>{{info.projects.length}}</p>
         </div>
-
-        <h4 class="mt-5">2020</h4>
-        <ul>
-          <li>- Clone Wars | After Effect</li>
-          <li>- MMI | After Effect</li>
-          <li>- Portfolio | Bootstrap web site</li>
-          <li>- Incamp social network | Community management</li>
-          <li>- Incamp strategic communication | Communication</li>
-          <li>- Incamp SEO and concurrents analysis | SEO</li>
-          <li>- Prototype MMI sweat | Illustrator</li>
-          <li>- Incamp prior study | Project leader</li>
-          <li>- Website Exercices | Gulp</li>
-          <li>- Website Exercices | Less</li>
-          <li>- Website Exercices | Stencil</li>
-          <li>- Website Exercices | Webpack</li>
-          <li>- Print Art Nouveau style | Illustrator, Clip Studio Paint</li>
-          <li>- Print Mackintosh style | Illustrator</li>
-          <li>- Print Bauhaus style | Illustrator</li>
-          <li>- Print Roycroft style | Illustrator</li>
-          <li>- Birthday Boy | Transmedia, Premiere Pro - Project leader</li>
-          <li>- Muy Bad Reise | Virtual Escape Game Multilanguage</li>
-          <li>- After University | Google Data Studio</li>
-          <li>- Clean Walk | Communication</li>
-          <li>- Incamp presentation | Communication, English</li>
-          <li>- Print advertising Babysitter.de | Deutsch</li>
-          <li>- Volkswagen Presentation | Deutsch</li>
-          <li>- Hiking | Wordpress website</li>
-          <li>- EA Presentation | English</li>
-          <li>- Indiglu | Adobe XD, Photoshop, Indesign</li>
-          <li>- Salem Witches | Wordpress website</li>
-          <li>- Défi 24h | HTML,CSS website</li>
-          <li>- Uncensored Library | Communication, Yoast</li>
-          <li>- Draw your TP | Illustrator</li>
-          <li>- Movie website | Php, JSON</li>
-          <li>- Stretchy | Php, JSON, MySQL - Project leader</li>
-          <li>- Sunset | Illustrator</li>
-          <li>- Random words | Adobe XD</li>
-          <li>- Gossip TV | Premiere, English</li>
-          <li>- Inktober 2020</li>
-          <li>- Fantasy Cupcakes | Adobe XD</li>
-        </ul>
-
-        <h4 class="mt-5">2019</h4>
-        <ul>
-          <li>- FLy New | HTML, CSS, JS</li>
-          <li>- Plastic Pollution | English</li>
-          <li>- Meeting illustration | Illustrator</li>
-          <li>- Searchers interview | Premiere</li>
-          <li>- Moon | Illustrator, JS</li>
-          <li>- Escape Game | Python, Pixel</li>
-          <li>- Inktober 2019</li>
-          <li>- Mermay 2019</li>
-          <li>- Infography DUT MMI</li>
-          <li>- Infography IMAC</li>
-          <li>- Озеро Байкал | Russian</li>
-          <li>- САНКТ-ПЕТЕРБУРГ | Russian</li>
-          <li>- Другой Гагарин | Russian</li>
-          <li>- Illustration 01 | High School Newspaper</li>
-          <li>- Illustration 02 | High School Newspaper</li>
-          <li>- Illustration 03 | High School Newspaper</li>
-          <li>- Leonardo Fibonacci Presentation</li>
-        </ul>
-
-        <h4 class="mt-5">2018</h4>
-        <ul>
-          <li>- Brighton Booklet | English</li>
-          <li>- Le monde d’Ampère</li>
-          <li>- Mermay 2019</li>
-          <li>- Travel Book</li>
-          <li>- Das Reisebuch</li>
-          <li>- Factory act</li>
-          <li>- Audio testimony of WWI</li>
-          <li>- The Austro-Hungarian Empire</li>
-          <li>- The RAF in the WW2</li>
-          <li>- Trip to England</li>
-          <li>- Пётр Ильич Чайковский</li>
-        </ul>
-
-        <h4 class="mt-5">2017</h4>
-        <ul>
-          <li>- Humanitarian Mission Leaflet</li>
-          <li>- Labyrinth | Python</li>
-          <li>- High School Video Presentation</li>
-          <li>- Die Welt Paper</li>
-          <li>- RAF WWII</li>
-          <li>- Doping control</li>
-        </ul>
-
-        <h4 class="mt-5">2016</h4>
-        <ul>
-          <li>- Superheroes in art</li>
-          <li>- Maliki’s Contest</li>
-          <li>- The main presidents of the USA</li>
-          <li>- The new school project</li>
-          <li>- RAF WWII</li>
-          <li>- The emigrate situation in Europe | IEP Science-Po Lille</li>
-        </ul>
-
-        <h4 class="mt-5">2015</h4>
-        <ul>
-          <li>- Comics in English</li>
-          <li>- Comics in Deutsch</li>
-          <li>- Radio Emission in Latin</li>
-        </ul>
-
-        <h4 class="mt-5">2014</h4>
-        <ul>
-          <li>- First Wordpress website</li>
-          <li>- Articles Middle School Newspaper</li>
-          <li>- Poems with Middle School Club</li>
-        </ul>
       </div>
    </div>
 
