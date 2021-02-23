44779751b34fcac025c733621b69339decaa6b74
Cécile <gaulph@gmail.com>
2021-02-23 22:26:03 +0100

    page fly new

--- src/components/project/Fly.vue
+++ src/components/project/Fly.vue
@@ -1,8 +1,80 @@
 <template>
  <div>
-   <div class="container">
-     Fly New
+    <div id="fly">
+     <b-container>
+       <b-row>
+         <b-col cols="12" sm="5" class="intro my-5">
+           <h1 class="my-5">Fly New</h1>
+           <div class="my-5"  v-for="info in donnees" :key="info.id">
+             <div class="my-5">
+                <b-row class="ml-1">
+                    <p class="cat2" v-for="cat in info.cat" :key="cat.id">{{cat.name}}</p>
+                </b-row>
+              </div>
+           </div>
+         </b-col>
+       </b-row>
+     </b-container>
    </div>
+   <b-container v-for="info in donnees" :key="info.id" class="my-5">
+     <b-row>
+       <b-col cols="12" sm="3" class="presentation">
+         <h2>Client</h2>
+          <p>{{info.client}}</p>
+       </b-col>
+
+       <b-col cols="12" sm="3" class="presentation">
+         <h2>Year</h2>
+          <p>{{info.year}}</p>
+       </b-col>
+
+        <b-col cols="12" sm="6">
+          <h3>Brief</h3>
+          <p>{{info.brief}}</p>
+       </b-col>
+     </b-row>
+
+      <b-row class="my-5">
+      
+     </b-row>
+     <h3>Languages / Software</h3>
+     <b-row class="my-3 ml-1">
+        <p v-for="ls in info.ls" :key="ls.id" class="cat">{{ls.name}}</p>
+     </b-row>
+
+    <b-row class="my-5">
+       <b-col cols="12" sm="6">
+          <img src="../../assets/fly-new2.jpg" alt="Mock-up" class="img-fluid my-4" style="max-width:400px">
+        </b-col>
+        <b-col cols="12" sm="6">
+          <img src="../../assets/fly-new3.jpg" alt="Mock-up" class="img-fluid my-4" style="max-width:400px">
+        </b-col>
+     </b-row>
+      
+    <div id="fly2">
+
+      <b-row class="my-5">
+        <b-col cols="12" sm="7">
+          <h3>Reflection</h3>
+          <p v-for="ref in info.reflection" :key="ref.id">{{ref.text}}</p>
+        </b-col>
+      </b-row>
+      
+      <div class="my-5">
+        <h3>Ressources</h3>
+        <b-row >
+          <b-col cols="12">
+            <img src="../../assets/fly-color.jpg" alt="Ressources" class="img-fluid my-4" >
+          </b-col>
+          <b-col cols="12">
+            <img src="../../assets/fly-icon.jpg" alt="Ressources" class="img-fluid my-4" >
+          </b-col>
+        </b-row>
+      </div>
+           
+    </div>
+
+   </b-container>
 
    <footer class="text-center mt-5">
     <p><a href="#">Mentions légales</a> & <a href="#">Crédits</a>  | © 2020 cecile-gaultier.fr</p>
@@ -22,7 +94,7 @@ export default {
 
   created(){
         //lecture json directement avec Axios (json dans static)
-        axios.get('static/all.json')
+        axios.get('static/fly.json')
         .then(function(response){
         //succès réponse
         console.log(response.data);
@@ -39,5 +111,28 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+#fly{
+  background-image: url("../../assets/fly-new-front.jpg");
+  content: "";
+  background-repeat: no-repeat;
+  margin-left: .5rem;
+  margin: 0;
+  background-position-y: top;
+  background-size: 100%;
+}
+
+#fly2 h3{
+  border-left: #af1f28 3px solid;
+  padding-left: .5rem;
+  margin-bottom: 3rem;
+  font-size: 2.5rem;
+  margin-top: 12rem;
+}
+
+.carousel-inner{
+  margin-top: 10rem;
+}
+
 
 </style>
